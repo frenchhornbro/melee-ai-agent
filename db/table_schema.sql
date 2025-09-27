99ddfcebf9ce8e042e2eb10bdfa0ea4808c18708
Hyrum Durfee <htdurfee@gmail.com>
2025-09-27 16:54:12 -0600

    update(db) corrected syntax, removed duplicates

--- db/table_schema.sql
+++ db/table_schema.sql
@@ -5,24 +5,20 @@ CREATE TABLE `Player`(
 );
 CREATE TABLE `PlayerRanking`(
     `player_id` BIGINT UNSIGNED NOT NULL,
-    `rank_list_id` BIGINT NOT NULL,
+    `rank_list_id` BIGINT UNSIGNED NOT NULL,
     `rank` INT NOT NULL,
-    PRIMARY KEY(`player_id`, `rank_list_id`),
-    CONSTRAINT `playerranking_rank_list_id_foreign` FOREIGN KEY(`rank_list_id`) REFERENCES `RankList`(`id`),
-    CONSTRAINT `playerranking_player_id_foreign` FOREIGN KEY(`player_id`) REFERENCES `Player`(`id`)
+    PRIMARY KEY(`player_id`, `rank_list_id`)
 );
 CREATE TABLE `Character`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL UNIQUE
 );
 CREATE TABLE `TierEntry`(
-    `character_id` BIGINT NOT NULL,
-    `list_id` BIGINT NOT NULL,
+    `character_id` BIGINT UNSIGNED NOT NULL,
+    `list_id` BIGINT UNSIGNED NOT NULL,
     `rank` TINYINT NOT NULL,
     `tier` VARCHAR(255) NULL,
-    PRIMARY KEY(`character_id`, `list_id`),
-    CONSTRAINT `tierentry_list_id_foreign` FOREIGN KEY(`list_id`) REFERENCES `TierList`(`id`),
-    CONSTRAINT `tierentry_character_id_foreign` FOREIGN KEY(`character_id`) REFERENCES `Character`(`id`)
+    PRIMARY KEY(`character_id`, `list_id`)
 );
 CREATE TABLE `RankList`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -30,14 +26,21 @@ CREATE TABLE `RankList`(
 );
 CREATE TABLE `Main`(
     `player_id` BIGINT UNSIGNED NOT NULL,
-    `character_id` BIGINT NOT NULL,
-    `rank_list_id` BIGINT NOT NULL,
-    PRIMARY KEY(`player_id`, `character_id`, `rank_list_id`),
-    CONSTRAINT `main_player_id_foreign` FOREIGN KEY(`player_id`) REFERENCES `Player`(`id`),
-    CONSTRAINT `main_character_id_foreign` FOREIGN KEY(`character_id`) REFERENCES `Character`(`id`),
-    CONSTRAINT `main_rank_list_id_foreign` FOREIGN KEY(`rank_list_id`) REFERENCES `RankList`(`id`)
+    `character_id` BIGINT UNSIGNED NOT NULL,
+    `rank_list_id` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY(`player_id`, `character_id`, `rank_list_id`)
 );
 CREATE TABLE `TierList`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `tier_date` DATE NOT NULL
 );
+ALTER TABLE `PlayerRanking`
+    ADD CONSTRAINT `playerranking_rank_list_id_foreign` FOREIGN KEY(`rank_list_id`) REFERENCES `RankList`(`id`),
+    ADD CONSTRAINT `playerranking_player_id_foreign` FOREIGN KEY(`player_id`) REFERENCES `Player`(`id`);
+ALTER TABLE `TierEntry`
+    ADD CONSTRAINT `tierentry_list_id_foreign` FOREIGN KEY(`list_id`) REFERENCES `TierList`(`id`),
+    ADD CONSTRAINT `tierentry_character_id_foreign` FOREIGN KEY(`character_id`) REFERENCES `Character`(`id`);
+ALTER TABLE `Main`
+    ADD CONSTRAINT `main_player_id_foreign` FOREIGN KEY(`player_id`) REFERENCES `Player`(`id`),
+    ADD CONSTRAINT `main_character_id_foreign` FOREIGN KEY(`character_id`) REFERENCES `Character`(`id`),
+    ADD CONSTRAINT `main_rank_list_id_foreign` FOREIGN KEY(`rank_list_id`) REFERENCES `RankList`(`id`)
